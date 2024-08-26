b3ff29e84df3f4df21a32094c7713af76b2f2539
Григорченко Петр <grigorchenko.p@compmechlab.ru>
2024-08-26 03:22:07 +0300

    counter

--- datagile-counter/typings/events.d.ts
+++
@@ -1,4 +0,0 @@
-type OnClick = React.MouseEvent<HTMLButtonElement | HTMLDivElement, MouseEvent>
-type OnChange = React.ChangeEvent<HTMLInputElement>
-type OnKeyDown = React.KeyboardEventHandler<HTMLElement>
-type OnBlur = React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
